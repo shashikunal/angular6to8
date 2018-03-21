d0dd9340ef1c3cef37c355fc1149dfedb7bc2d69
shashikunal <shahsikunal@gmail.com>
2018-03-21 19:31:43 +0530

    test

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 var testApp = angular.module("myApp" , []);
 
-testApp.controller("myCtr" , ($scope , $http)=>{
-$http.get("https://api.github.com/users").then((item)=>{
-$scope.users = item.data;
+
+
+
+testApp.controller("myCtr" , function($scope , $http){
+
+        $scope.deletePlayer = function(player){
+        var removedPlayer = $scope.players.indexOf(player);
+        $scope.players.splice(removedPlayer , 1);
+    
+    }
+
+
+    $scope.addPlayers = function(){
+        $scope.players.unshift({
+            name: $scope.newName,
+            id: $scope.newId,
+            runs:$scope.newRuns
+        });
+    };  
+
+$http.get("data.json").then(function(item){
+$scope.players = item.data;
 });
 });
 
