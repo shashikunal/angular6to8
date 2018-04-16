89837d652660ac4042e947b974a1569f85e8914f
shashikunal <shahsikunal@gmail.com>
2018-04-16 19:16:27 +0530

    hello

--- app.js
+++ app.js
@@ -52,4 +52,54 @@ myApp.config(["$routeProvider" , "$locationProvider" , function($routeProvider ,
     }).otherwise({
         redirectTo:"/login"
     });
-}])
+}]);
+
+
+
+
+
+//new data
+myApp.run(function($rootScope){
+    $rootScope.message = "hello this is root scope";
+})
+//service 
+myApp.service("myService" , function(){
+    var num = Math.round(Math.random()*100);
+     this.random = function(){
+        return num;
+    }
+});
+
+//factory
+myApp.factory("myFactory" , function(){
+    var obj = {};
+    var num = Math.round(Math.random()*100);
+    obj.random = function(){
+        return num;
+    }
+    return obj;
+});
+
+myApp.controller("myCtr" , function($scope , myService , $location , $timeout ,$interval ) {
+    $scope.randomNumber = function(){
+      $scope.randomcheck = myService.random();
+    }
+
+    $scope.MyLocation = $location.absUrl();
+
+    $scope.test = "hello java";
+
+    $timeout(function(){
+        $scope.test = "hello angularjs";
+    },2000);
+
+
+    $scope.time = new Date().toLocaleTimeString();
+    $interval(function(){
+        $scope.time = new Date().toLocaleTimeString();
+    },1000);
+
+   $scope.message = "hello this is  scope";
+});
+
+ 
